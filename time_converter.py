da012af0a4e75a77c4a00588eae2a71c5f8812ed
KennedyShiku <kenrichshiku@gmail.com>
2023-12-09 13:13:34 +0300

    Added variables for stringed hour and minute

--- time_converter.py
+++ time_converter.py
@@ -1,7 +1,12 @@
-def twenty_four_hour(input_hour, period):
+def twenty_four_hour(input_hour, input_minute, period):
     if period == "am" and input_hour == 12:
         input_hour = 0
     elif period == "pm" and input_hour != 12:
         input_hour += 12
-    return input_hour
+    
+    new_hour = str(input_hour)
+    new_minute = str(input_minute)
+    
+     
+    # return input_hour
 print(twenty_four_hour(1, "pm"))
