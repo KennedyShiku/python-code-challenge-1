81a8546b9b75ab45a54567a34d529fc5bc6ce8bb
KennedyShiku <kenrichshiku@gmail.com>
2023-12-09 13:28:02 +0300

    Added 0 in order for final time to be four digits

--- time_converter.py
+++ time_converter.py
@@ -7,6 +7,10 @@ def twenty_four_hour(input_hour, input_minute, period):
     new_hour = str(input_hour)
     new_minute = str(input_minute)
     
-     
+    if new_hour < 10:
+        new_hour = "0" + new_hour
+    if new_minute < 10:
+        new_minute = "0" + new_minute
+    
     # return input_hour
 print(twenty_four_hour(1, "pm"))
